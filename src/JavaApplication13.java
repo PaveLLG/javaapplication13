59179c85261530e8181bc254936c758155984eb6
bpavel07@yandex.ru <gggtttttHHHHHH7889787>
2021-09-02 17:33:29 +0300

    1231

--- src/JavaApplication13.java
+++ src/JavaApplication13.java
@@ -1,7 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
 public class JavaApplication13 {
 
     public static void main(String[] args) {
-        
+
+        File file   = new File("text.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
     }
 
 }
